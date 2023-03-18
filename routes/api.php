964463f21083cd2c413faa0b5ffd8fce0d1bddeb
Hassan Tahir <hassantahirjaura@gmail.com>
2023-03-18 20:04:36 +0500

    Set up api token generation

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\SanctumController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::post('/sanctum/token', [SanctumController::class, 'token']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
